945ea4aeeea4bb3fe08116275ea4d892eb003170
airfight <745756619@qq.com>
2017-02-06 16:20:42 +0800

    //

---
+++ GYVideo/GYVideo/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  GYVideo
+//
+//  Created by zhuguangyang on 2017/2/6.
+//  Copyright © 2017年 GYJade. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let player = AVPlayer(url: URL(string:"http://yyygwz.com/index.php?url=https://v.qq.com/x/cover/jk97cgptoachp45/o0022kxbsz4.html")!)
+        
+        let playerVc = AVPlayerViewController()
+        
+        playerVc.player = player
+        self.addChildViewController(playerVc)
+        self.view.addSubview(playerVc.view)
+        playerVc.view.frame = view.frame
+        
+        player.play()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
