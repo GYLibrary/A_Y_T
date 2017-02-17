179e46d6a1688bdfa22acfe302b203e46780f875
airfight <745756619@qq.com>
2017-02-17 13:44:22 +0800

    实例化网络工具

---
+++ GYVideo/GYVideo/Tools/NetWork/GYNetWorkManager.swift
@@ -0,0 +1,24 @@
+//
+//  GYNetWorkManager.swift
+//  GYVideo
+//
+//  Created by ZGY on 2017/2/17.
+//  Copyright © 2017年 GYJade. All rights reserved.
+//
+//  Author:        Airfight
+//  My GitHub:     https://github.com/airfight
+//  My Blog:       http://airfight.github.io/
+//  My Jane book:  http://www.jianshu.com/users/17d6a01e3361
+//  Current Time:  2017/2/17  13:40
+//  GiantForJade:  Efforts to do my best
+//  Real developers ship.
+
+import UIKit
+
+class GYNetWorkManager: NSObject {
+    
+    static let shareNetWorkTool = GYNetWorkManager()
+
+}
+
+
